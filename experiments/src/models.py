ad05866860b87f51549b767c93824c6a29a64df4
Felix Qvist <felix.qvist@gmail.com>
2023-03-14 14:01:01 +0100

    fix num_classes

--- experiments/src/models.py
+++ experiments/src/models.py
@@ -5,29 +5,29 @@ import torch
 
 def get_inception_model():
     model = models.inception_v3()
-    model.AuxLogits.fc = nn.Linear(768, 1)
-    model.fc = nn.Linear(2048, 1)
+    model.AuxLogits.fc = nn.Linear(768, 2)
+    model.fc = nn.Linear(2048, 2)
 
     optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4)
 
     return model, optimizer, 30
 
 def get_xception_model():
-    model = timm.create_model('xception', pretrained=False, num_classes=1)
+    model = timm.create_model('xception', pretrained=False, num_classes=2)
 
     optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4)
 
     return model, optimizer, 30
 
 def get_densenet_model():
-    model = timm.create_model('densenet121', pretrained=False, num_classes=1)
+    model = timm.create_model('densenet121', pretrained=False, num_classes=2)
 
     optimizer = torch.optim.RMSprop(model.parameters(), lr=1e-4)
 
     return model, optimizer, 30
 
 def get_vit_model():
-    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=1)
+    model = timm.create_model('vit_base_patch16_224', pretrained=False, num_classes=2)
 
     optimizer = torch.optim.Adam(model.parameters(), lr=1e-5)
 
