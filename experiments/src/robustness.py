81c2bfe107af55233f5e5c827dc2021a985d9a65
Felix Qvist <felix.qvist@gmail.com>
2023-03-22 16:07:32 +0100

    robustness experiments

--- experiments/src/robustness.py
+++ experiments/src/robustness.py
@@ -1,16 +1,32 @@
 import torch
+import numpy as np
+from torchvision import transforms
 from src.dataloader import load_dataset
 from src.experiment import sdd_path, REAL_LOC
-from src.util import get_accuracy, get_device
+from src.util import get_accuracy, get_device, JPEGcompression
+from cleverhans.torch.attacks.projected_gradient_descent import (
+    projected_gradient_descent,
+)
+from tqdm import tqdm
 
-CH_EPSILONS = [0.01, 0.02, 0.04, 0.08, 0.16, 0.32, 0.64, 1.28, 2.56, 5.12, 10.24, 20.48]
+CH_EPSILONS = [0.01, 0.02, 0.04, 0.08, 0.16, 0.32]
 
-def test_robustness(
-    model_func, model_name: str, size: int, sdd_version : str
-    ):
-    name = (
-        f"{model_name}_{sdd_version}"
-    )
+TRANSFORMS = {
+    "pad3": transforms.Pad(padding=3),
+    "pad10": transforms.Pad(padding=10),
+    "pad30": transforms.Pad(padding=30),
+    "pad50": transforms.Pad(padding=50),
+    "compress": transforms.Lambda(JPEGcompression),
+    "grayscale": transforms.Grayscale(3),
+    "colorjitter": transforms.ColorJitter(
+        brightness=0.1, hue=0.01, saturation=0.1, contrast=0.1
+    ),
+    "randomaffine": transforms.RandomAffine(degrees=(0, 10), translate=(0, 0.15)),
+}
+
+
+def test_robustness(model_func, model_name: str, size: int, sdd_version: str):
+    name = f"{model_name}_{sdd_version}"
 
     print("TESTING ROBUSTNESS FOR: ", name)
     device = get_device()
@@ -35,7 +51,7 @@ def test_robustness(
         print("DATASET LOADED")
 
         test_acc = 0.0
-        for i, (images, labels) in enumerate(test_loader):
+        for images, labels in tqdm(test_loader):
             images = images.to(device)
             labels = labels.reshape((labels.shape[0])).to(device)
             output = torch.softmax(model(images), dim=1)
@@ -43,6 +59,29 @@ def test_robustness(
         test_accuracy = test_acc / len(test_loader)
         print(f"TEST ACCURACY ON {sdv}: ", test_accuracy)
         evaluation_results[sdv] = test_accuracy
+
+    print()
+    print("TESTING PERFORMANCE ON TRANSFORMS")
+    for t in TRANSFORMS.keys():
+        print("TESTING ON: ", t)
+        train_loader, test_loader = load_dataset(
+            real_path=REAL_LOC,
+            fake_path=sdd_path[sdd_version],
+            batch_size=32,
+            samples=50000,
+            size=size,
+            extra_transforms=TRANSFORMS[t],
+        )
+        print("DATASET LOADED")
+        test_acc = 0.0
+        for images, labels in tqdm(test_loader):
+            images = images.to(device)
+            labels = labels.reshape((labels.shape[0])).to(device)
+            output = torch.softmax(model(images), dim=1)
+            test_acc += get_accuracy(output, labels, labels.shape[0])
+        test_accuracy = test_acc / len(test_loader)
+        print(f"TEST ACCURACY ON {t}: ", test_accuracy)
+        evaluation_results[t] = test_accuracy
 
     print()
     print("TESTING PERFORMANCE ON CLEVER HANS ATTACKS")
@@ -58,13 +97,15 @@ def test_robustness(
         print("DATASET LOADED")
 
         test_acc = 0.0
-        for i, (images, labels) in enumerate(test_loader):
+        for images, labels in tqdm(test_loader):
             images = images.to(device)
+            x_pgm = projected_gradient_descent(
+                model, images, eps, (2.5 * eps) / 40, 40, np.inf
+            )
             # CH attack
             labels = labels.reshape((labels.shape[0])).to(device)
-            output = torch.softmax(model(images), dim=1)
+            output = torch.softmax(model(x_pgm), dim=1)
             test_acc += get_accuracy(output, labels, labels.shape[0])
         test_accuracy = test_acc / len(test_loader)
         print(f"TEST ACCURACY ON {eps}: ", test_accuracy)
         evaluation_results[eps] = test_accuracy
-
