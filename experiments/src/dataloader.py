4019c0b9e783da4c2ee0aa509493d5bdb0c72cb4
Felix Qvist <felix.qvist@gmail.com>
2023-03-22 10:42:02 +0100

    fix warning

--- experiments/src/dataloader.py
+++ experiments/src/dataloader.py
@@ -43,7 +43,7 @@ def get_transforms(size: int):
     transform = transforms.Compose(
         [
             transform,
-            transforms.Resize((size, size)),
+            transforms.Resize((size, size), antialias=None),
             transforms.Normalize(
                 mean=(0.48145466, 0.4578275, 0.40821073),
                 std=(0.26862954, 0.26130258, 0.27577711),
